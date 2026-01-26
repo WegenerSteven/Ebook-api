cdff9c45979bb7666514b34556e2ee389ccf4239
WegenerSteven <wegenersteven580@gmail.com>
2026-01-27 00:56:40 +0300

    refactor: standardize formatting in DTO files for consistency

--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -2,14 +2,14 @@ import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ResetPasswordDto {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    token: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  token: string;
 
-    @ApiProperty({ minLength: 6 })
-    @IsString()
-    @MinLength(6)
-    @IsNotEmpty()
-    newPassword: string;
+  @ApiProperty({ minLength: 6 })
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  newPassword: string;
 }
