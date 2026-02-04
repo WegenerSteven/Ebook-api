55dc13710d13f44955840f41d790a5202db16139
WegenerSteven <wegenersteven580@gmail.com>
2026-02-04 13:54:18 +0300

    feat: add DatabaseService to HealthCheckModule providers

--- src/health-check/health-check.module.ts
+++ src/health-check/health-check.module.ts
@@ -4,10 +4,11 @@ import { HealthCheckController } from './health-check.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { User } from '../entities';
+import { DatabaseService } from '../config/database/database.service';
 
 @Module({
   imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([User])],
   controllers: [HealthCheckController],
-  providers: [HealthCheckService],
+  providers: [HealthCheckService, DatabaseService],
 })
 export class HealthCheckModule {}
