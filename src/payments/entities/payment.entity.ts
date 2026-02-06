9122940c6899b49d8b3ef8fe82d12de7d0d09a78
WegenerSteven <wegenersteven580@gmail.com>
2026-02-06 17:49:57 +0300

    feat: implement Payment entity with properties and relationships for payment processing

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,1 +1,100 @@
-export class Payment {}
+import {
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../entities/user.entity';
+import { Order } from '../../entities/order.entity';
+
+export enum PaymentStatus {
+  PENDING = 'pending',
+  PROCESSING = 'processing',
+  SUCCEEDED = 'succeeded',
+  FAILED = 'failed',
+  REFUNDED = 'refunded',
+  CANCELLED = 'cancelled',
+}
+
+export enum PaymentMethod {
+  STRIPE = 'stripe',
+  MPESA = 'mpesa',
+}
+
+@Entity('payments')
+export class Payment {
+  @PrimaryGeneratedColumn()
+  paymentId: string;
+
+  @Column({ unique: true })
+  paymentIntentId: string; //string PaymentIntent ID
+
+  @Column()
+  amount: number;
+
+  @Column()
+  currency: string;
+
+  @Column({
+    type: 'enum',
+    enum: PaymentStatus,
+    default: PaymentStatus.PENDING,
+  })
+  status: PaymentStatus;
+
+  @Column({
+    type: 'enum',
+    enum: PaymentMethod,
+    default: PaymentMethod.STRIPE,
+  })
+  method: PaymentMethod;
+
+  // @Column({
+  //   nullable: true,
+  // })
+  // stripeCustomerId?: string;
+
+  @Column({ nullable: true })
+  receiptUrl?: string;
+
+  @Column({ type: 'jsonb', nullable: true })
+  metadata?: Record<string, any>;
+
+  @Column({ nullable: true })
+  failureReason?: string;
+
+  @Column({ nullable: true })
+  clientSecret?: string;
+
+  @Column({ nullable: true })
+  stripeCustomerId?: string;
+
+  @ManyToOne(() => User, (user) => user.payments)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => Order)
+  @JoinColumn({ name: 'orderId' })
+  order: Order;
+
+  @Column()
+  orderId: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
