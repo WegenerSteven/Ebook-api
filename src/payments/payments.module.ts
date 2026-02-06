245cf16c96f84e4679bd85bc94ffe3e87e1730ad
WegenerSteven <wegenersteven580@gmail.com>
2026-02-06 17:48:53 +0300

    feat: integrate WebhookController and update module imports in PaymentsModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from '../entities/order.entity';
+import { ConfigModule } from '@nestjs/config';
+import { Payment, User } from '../entities';
+import { WebhookController } from './webhook.controller';
 
 @Module({
-  controllers: [PaymentsController],
+  imports: [TypeOrmModule.forFeature([Order, Payment, User]), ConfigModule],
+  controllers: [PaymentsController, WebhookController],
   providers: [PaymentsService],
+  exports: [PaymentsService],
 })
 export class PaymentsModule {}
